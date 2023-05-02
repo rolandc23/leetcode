74a8c91d7b8c5e74bafcb439e558f0fa9d422203
rolandc23 <132192332+rolandc23@users.noreply.github.com>
2023-05-03 04:47:07 +1000

    Update jumpGame.py

--- jumpGame.py
+++ jumpGame.py
@@ -1,3 +1,10 @@
+# You are given an integer array nums.
+# You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+# https://leetcode.com/problems/jump-game/
+
+Return true if you can reach the last index, or false otherwise.
+
 def solution(array):
     fuel = 0;
     for i in array:
