664df85ded3b0f78c3c4bd26dc193790abfc3023
rolandc23 <132192332+rolandc23@users.noreply.github.com>
2023-05-03 04:48:02 +1000

    Update rainwater.py

--- rainwater.py
+++ rainwater.py
@@ -1,3 +1,7 @@
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+# https://leetcode.com/problems/trapping-rain-water/
+
 def solution(height):
     size = len(height);
     maxHeight = 0;
