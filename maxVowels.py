879158befd32b0a4641fd5cf8949ea0ac5f2a207
rolandc23 <132192332+rolandc23@users.noreply.github.com>
2023-06-24 13:56:40 +1000

    Update and rename maxVowels to maxVowels.py

--- maxVowels.py
+++ maxVowels.py
@@ -1,3 +1,6 @@
+# Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+# https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length
+
 class Solution(object):
 
     def maxVowels(self, s, k):
